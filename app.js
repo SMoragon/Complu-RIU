74dd4e705e9044e23bf4c89b141dfc19488c8c80
Zhi <zhipengg@ucm.es>
2023-11-13 19:30:33 +0100

    Start project
    Not pizza todays D:🍕
    
    Co-Authored-By: SMoragon <145195231+SMoragon@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,41 +1,43 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const port = 3000;
+
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+// Enrutamiento de las páginas principales de nuestra aplicación.
+app.get("/", (request, response) => {
+  response.status(200).render("index.ejs");
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get("/index.html", (request, response) => {
+  response.status(200).render("index.ejs");
+});
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  res.status(404).send("<h1>ERROR 404 </h1>")
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+
+// Función para iniciar el servidor, que espera las peticiones del usuario.
+app.listen(port, (err) => {
+  if (err) {
+    console.log("An error ocurred while listening to server");
+  } else {
+    console.log(`Server listening on port http://localhost:${port} `);
+  }
 });
 
 module.exports = app;
